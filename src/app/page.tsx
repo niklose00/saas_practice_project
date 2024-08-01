ffeae90d0c11a484b37ba7ecc8300ab5e4b9f0f9
Nico <klosenico00@gmail.com>
2024-08-01 13:40:26 +0200

    refactor: Application name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit itmes-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 ">
           <p className="text-sm font-semibold text-gray-700">
-            Quill is now public
+            firstSaaS is now public
           </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
           Chat with your <span className="text-blue-600">documents</span> in seconds.
         </h1>
-        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">Quill allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away.</p>
+        <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">firstSaaS allows you to have conversations with any PDF document. Simply upload your file and start asking questions right away.</p>
         <Link className={buttonVariants({ size: "lg", className: "mt-5" })} href="/dashboard" target="_blank">Get started <ArrowRight className="ml-2 h-5 w-5"></ArrowRight></Link>
       </MaxWidthWrapper>
 
@@ -64,7 +64,7 @@ export default function Home() {
             </h2>
             <p className='mt-4 text-lg text-gray-600'>
               Chatting to your PDF files has never been
-              easier than with Quill.
+              easier than with firstSaaS.
             </p>
           </div>
         </div>
@@ -114,7 +114,7 @@ export default function Home() {
                 Start asking questions
               </span>
               <span className='mt-2 text-zinc-700'>
-                It&apos;s that simple. Try out Quill today -
+                It&apos;s that simple. Try out firstSaaS today -
                 it really takes less than a minute.
               </span>
             </div>
